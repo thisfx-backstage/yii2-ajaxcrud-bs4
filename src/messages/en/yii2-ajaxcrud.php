aa8d71d665d723b2cbae97acad3c9d52284c4953
biladina <nabil_munawar@protonmail.ch>
2020-10-04 21:54:03 +0700

    add translation english and indonesia

---
+++ src/messages/en/yii2-ajaxcrud.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Message translations for yii2-ajaxcrud.
+ *
+ * 
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+return [
+    'Create' => 'Create',
+    'Create New' => 'Create New',
+    'Create More' => 'Create More',
+    'Update' => 'Update',
+    'Delete' => 'Delete',
+    'Close' => 'Close',
+    'Save' => 'Save',
+    'View' => 'View',
+    'Success' => 'success',
+    'Reset Grid' => 'Reset Grid',
+    'Resize Column' => 'Resize table columns just like a spreadsheet by dragging the column edges',
+    'Delete All' => 'Delete All',
+    'Delete Confirm' => 'Are you sure want to delete this item?'
+];
---
+++ src/messages/id/yii2-ajaxcrud.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Message translations for yii2-ajaxcrud.
+ *
+ * 
+ * NOTE: this file must be saved in UTF-8 encoding.
+ */
+return [
+    'Create' => 'Tambah',
+    'Create New' => 'Tambah',
+    'Create More' => 'Tambah Lagi',
+    'Update' => 'Ubah',
+    'Delete' => 'Hapus',
+    'Close' => 'Tutup',
+    'Save' => 'Simpan',
+    'View' => 'Lihat',
+    'Success' => 'Berhasil',
+    'Reset Grid' => 'Muat Ulang',
+    'Resize Column' => 'Ukuran tabel bisa diubah seperti di excel/spreadsheet dengan menarik tepi kolom',
+    'Delete All' => 'Hapus',
+    'Delete Confirm' => 'Anda yakin ingin menghapus data ini?'
+];
